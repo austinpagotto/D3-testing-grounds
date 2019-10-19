fd5c3061d315c2419799bf5c7f25a5817173a5f2
Austin Pagotto <austinpagotto@gmail.com>
2019-10-19 15:06:05 -0400

    nested pattern

--- fruitbowl.js
+++ fruitbowl.js
@@ -6,21 +6,24 @@ const radiusScale = d3.scaleOrdinal()
 .domain(['apple','lemon'])
 .range(['50','30']);
 
-const xPosition = (d,i)=> i *120 + 60;
+// const xPosition = (d,i)=> i *120 + 60;
 const fruitBowl = (selection,{fruits}) => {
-    const circles = selection.selectAll('circle')
-    .data(fruits,d => d.id);
 
-    circles.enter().append('circle')
-      .attr('cx',xPosition)
-      .attr('cy',height/2)
-      .attr('r',0)
-    .merge(circles)
+  const groups = selection.selectAll('g')
+  .data(fruits,d => d.id);
+
+ const groupsEnter =  groups.enter().append('g');
+  groupsEnter.merge(groups)
+  .attr('transform',(d,i) => `translate(${i *180 + 100},${height/2})`);
+    groups.exit().remove();
+
+    groupsEnter.append('circle')
+    .merge(groups.select('circle'))
       .attr('fill',d => colorScale(d.type))
-    .transition().duration(1000)
-      .attr('cx',xPosition)
       .attr('r',d => radiusScale(d.type));
 
-    circles.exit().transition().duration(1000).attr('r',0)
-    .remove();
+      groupsEnter.append('text')
+      .merge(groups.select('text'))
+      .text(d => d.type)
+      .attr('y',120);
   }
