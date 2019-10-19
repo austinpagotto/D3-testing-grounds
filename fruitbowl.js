64bfe3f06db8ea1c7e89359ee6fde4d403e39aaa
Austin Pagotto <austinpagotto@gmail.com>
2019-10-19 14:27:09 -0400

    transitions

--- fruitbowl.js
+++ fruitbowl.js
@@ -6,16 +6,21 @@ const radiusScale = d3.scaleOrdinal()
 .domain(['apple','lemon'])
 .range(['50','30']);
 
+const xPosition = (d,i)=> i *120 + 60;
 const fruitBowl = (selection,{fruits}) => {
     const circles = selection.selectAll('circle')
-    .data(fruits);
+    .data(fruits,d => d.id);
 
     circles.enter().append('circle')
-    .attr('cx',(d,i)=> i *120 + 60)
-    .attr('cy',height/2)
+      .attr('cx',xPosition)
+      .attr('cy',height/2)
+      .attr('r',0)
     .merge(circles)
-    .attr('r',d => radiusScale(d.type))
-    .attr('fill',d => colorScale(d.type));
+      .attr('fill',d => colorScale(d.type))
+    .transition().duration(1000)
+      .attr('cx',xPosition)
+      .attr('r',d => radiusScale(d.type));
 
-    circles.exit().remove();
+    circles.exit().transition().duration(1000).attr('r',0)
+    .remove();
   }
--- generalUpdate.js
+++ generalUpdate.js
@@ -4,9 +4,12 @@ function setbkrd() {
 
   window.height = svg.attr("height");
   
-  const makeFruit = type => ({type});
+  const makeFruit = type => ({
+    type,
+    id:Math.random()
+  });
 
-  const fruits = d3.range(5)
+  let fruits = d3.range(5)
   .map(() => makeFruit('apple'));
 
   fruitBowl(svg,{fruits});
@@ -20,6 +23,11 @@ function setbkrd() {
     fruits[2].type = 'lemon';
     fruitBowl(svg,{fruits});
   },2000)
+
+  setTimeout(()=> {
+    fruits=fruits.filter((d,i)=> i != 1)
+    fruitBowl(svg,{fruits});
+  },3000)
 }
 
 window.onload = setbkrd;
